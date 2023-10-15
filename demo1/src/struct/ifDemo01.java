fc84627741c9e1bd3d36d9ce846befe75038b99e
nanluoguxiang <76835780+nanluoguxiang@users.noreply.github.com>
2023-10-15 20:14:28 +0800

    create a remote test git responsitory

---
+++ demo1/src/struct/ifDemo01.java
@@ -0,0 +1,20 @@
+package struct;
+
+import java.util.Scanner;
+
+public class ifDemo01 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入内容：");
+        String s = scanner.nextLine();
+
+
+
+        if(s.equals("Hello")){
+            System.out.println(s);
+        }
+
+        System.out.println("End");
+        scanner.close();
+    }
+}
